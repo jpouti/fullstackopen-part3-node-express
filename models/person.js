55d18210df8fb0ee1d603b8b68d11ad188122518
Juho Poutiainen <j.poutiainen94@gmail.com>
2022-06-12 23:15:41 +0300

    Finished phonebook backend -> solutions for - 3.22

--- models/person.js
+++ models/person.js
@@ -1,51 +1,52 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URL;
+const url = process.env.MONGODB_URL
 
 mongoose.connect(url)
-    .then(result => {
-        console.log('Connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB')
-    });
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB', error)
+  })
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minlength: 3,
-        required: true
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    validate: {
+    //Valid forms eg. ['12345678'] & [01-234567'] & ['010-23456']
+      validator: function(v) {
+        for (let index = 4; index < v.length; index++) {
+          if(v[index] === '-') {
+            console.log(v[index])
+            return false
+          }
+        }
+        console.log(v[1])
+        if (v[1] === '-') {
+          return false
+        }
+
+        return /\d{8}/
+      },
+      message: props => `${props.value} is not a valid phone number! Valid forms eg. ['12345678'] & [01-234567'] & ['010-23456']`
     },
-    number: {
-        type: String,
-        minlength: 8,
-        validate: {
-            validator: function(v) {
-                for (let index = 4; index < v.length; index++) {
-                    if(v[index] === "-") {
-                        console.log(v[index])
-                        return false;
-                    }
-                }
-                console.log(v[1]);
-                if (v[1] === "-") {
-                    return false;
-                }
-                
-                return /\d{8}/;
-            },
-            message: props => `${props.value} is not a valid phone number! Valid forms eg. ['12345678'] & [01-234567'] & ['010-23456']`
-        },
-        required: [true, 'User phone number required']
-    }
-});
+    required: [true, 'User phone number required']
+  }
+})
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
-});
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
-module.exports = mongoose.model('Person', personSchema);
+module.exports = mongoose.model('Person', personSchema)
